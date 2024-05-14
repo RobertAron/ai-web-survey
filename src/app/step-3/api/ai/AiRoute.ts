9ca73ca11f0fb31372c6a798444d9714dd40b3d4
jfisher52 <jrfish@uw.edu>
2024-05-14 09:41:13 -0700

    Update AiRoute.ts

--- src/app/step-3/api/ai/AiRoute.ts
+++ src/app/step-3/api/ai/AiRoute.ts
@@ -81,7 +81,7 @@ where
       .with(
         "Control",
         () =>
-          `Respond as a generic American. As such you feel neutral about ${question.topic}. Do not mention 'generic' or 'American' or other related words. Avoid speaking in first person ('I', 'We', etc.). Response briefly.`
+          `Respond as a neutral US citizen. As such you feel neutral about ${question.topic}. Do not mention 'neutral' or 'American' or other related words. Avoid speaking in first person ('I', 'We', etc.). Response briefly.`
       )
       .exhaustive();
 
